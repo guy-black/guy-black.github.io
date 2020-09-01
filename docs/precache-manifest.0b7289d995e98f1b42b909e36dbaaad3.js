f4bbe49517f3fd0d6c010f8a58ca2e794f9969e9
guy-black <67166723+guy-black@users.noreply.github.com>
2020-09-01 14:32:52 -0500

    build

---
+++ docs/precache-manifest.0b7289d995e98f1b42b909e36dbaaad3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d62aaa21489e04aa6f4799399adbd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d62b81fb4ca53aa98238",
+    "url": "/static/css/main.d6e54427.chunk.css"
+  },
+  {
+    "revision": "8934fa38d003629eff50",
+    "url": "/static/js/2.71caf9cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.71caf9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d62b81fb4ca53aa98238",
+    "url": "/static/js/main.3fae5d47.chunk.js"
+  },
+  {
+    "revision": "c90b411c68a14278722b",
+    "url": "/static/js/runtime-main.5d119ae5.js"
+  },
+  {
+    "revision": "25104e31cd9acf9e7b669f4eb200925b",
+    "url": "/static/media/logo.25104e31.svg"
+  },
+  {
+    "revision": "10557038f208db62e50bce06ab4ad901",
+    "url": "/static/media/logo192.10557038.png"
+  }
+]);
